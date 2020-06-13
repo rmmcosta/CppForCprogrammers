afa5149f2d1b66555f9fcb186f27205fd44c5632
rmmcosta <ricardocosta101085@gmail.com>
2020-06-13 17:08:52 +0100

    final exam

---
+++ week 5/q2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 2, b = -3, c = 2;
+
+    bool tval1 = false, tval2 = true;
+
+    char ch = 'b';
+    cout << b + c * a << endl;
+    cout << c%5/2 << endl;
+    cout << -a * c++ << endl;
+    cout << (tval1 && tval2) << endl;
+    cout << (ch += 2) << endl;
+    cout << c%5 << endl;
+    cout << 3/2 << endl;
+    return 0;
+}
