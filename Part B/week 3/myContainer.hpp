a549579315e9564e3b4b2a705959028b02bc7cbf
rmmcosta <ricardocosta101085@gmail.com>
2020-07-10 08:50:27 +0100

    my container and constructor types (default, copy and move)

--- Part B/week 3/myContainer.hpp
+++ Part B/week 3/myContainer.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 #ifndef MYCONTAINER_HPP
 #define MYCONTAINER_HPP
 template <typename T, int n>
@@ -5,8 +7,64 @@ class MyContainer
 {
 public:
     MyContainer() { a = new T[n]; }
+    explicit MyContainer(T *b) : MyContainer()
+    {
+        cout << "explicit constructor" << endl;
+        for (int i = 0; i < n; i++)
+            a[i] = b[i];
+    } //with explicit we don't have conversions
+    MyContainer(const MyContainer &arr) : MyContainer()
+    {
+        cout << "copy constructor" << endl;
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = arr[i];
+        }
+
+    } //copy constructor
+    MyContainer(MyContainer &&b) noexcept
+    {
+        a = b.a;
+        b.a = nullptr;
+    } //move constructor (we also need to overload the assignment)
     ~MyContainer() { delete[] a; }
-    int getSize() const;
+    int getSize() const { return n; };
+
+    void swap(MyContainer &b)
+    {
+        MyContainer temp = move(b); //uses the overridden op= defined for move semantics
+        b = move(*this);
+        *this = move(temp);
+    } //efficient swap -> o(1)
+
+    T *begin()
+    {
+        return a;
+    }
+
+    T *end()
+    {
+        return a + n;
+    }
+
+    T &operator[](int i) { return a[i]; };
+    T operator[](int i) const { return a[i]; }; //we need this for instance for the copy constructor
+    //it's the read only cousin of the previous one
+    MyContainer &operator=(MyContainer &&b) noexcept
+    {
+        a = b.a;
+        b.a = nullptr;
+        return *this; //we need to return the object itself not a pointer
+    }
+
+    friend ostream &operator<<(ostream &out, MyContainer &arr)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            out << (arr[i] == '\0' ? '-' : arr[i]) << " ";
+        }
+        return out;
+    }
 
 private:
     T *a;
--- Part B/week 3/myContainerTest.cpp
+++ Part B/week 3/myContainerTest.cpp
@@ -1,7 +1,44 @@
 #include "myContainer.hpp"
+#include <iostream>
+using namespace std;
 
 int main()
 {
     MyContainer<char, 3> chars;
+    cout << "size " << chars.getSize() << endl;
+    chars[0] = 'A';
+    chars[1] = 'n';
+    chars[2] = 'a';
+    cout << "chars " << chars << endl;
+    MyContainer<char, 3> chars2(chars); //in order to this work fine
+    //we need to define the copy constructor. If we don't do it
+    //a copy constructor will be auto generated and only the private pointer will be assigned
+    cout << chars2 << endl;
+    char myName[7] = {'R', 'i', 'c', 'a', 'r', 'd', 'o'};
+    cout << "char array myName " << myName << endl;
+    MyContainer<char, 5> chars3(myName);
+    cout << "chars3 " << chars3 << endl;
+    MyContainer<char, 3> chars4;
+    chars4 = move(chars); //uses the move constructor
+    cout << "chars4 " << chars4 << endl;
+    //cout << chars << endl;//chars doesn't exist anymore
+    MyContainer<char, 3> aei;
+    aei[0] = 'a';
+    aei[1] = 'e';
+    aei[2] = 'i';
+    MyContainer<char, 3> oua;
+    oua[0] = 'o';
+    oua[1] = 'u';
+    oua[2] = 'a';
+    cout << "aei " << aei << endl;
+    cout << "oua " << oua << endl;
+    aei.swap(oua);
+    cout << "aei " << aei << endl;
+    cout << "oua " << oua << endl;
+    for (auto iter = aei.begin(); iter != aei.end(); iter++)
+    {
+        cout << "inside iter " << *iter << endl;
+    }
+
     return 0;
 }
